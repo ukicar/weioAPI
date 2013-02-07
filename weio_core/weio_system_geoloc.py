32d9af215744ed66818eb85222b3c900f8cd0ec4
uros petrevski <uros@nodesign.net>
2013-02-07 23:37:57 +0100

    added and tested function for finding IP and geolocalisation

--- weio_core/weio_system_geoloc.py
+++ weio_core/weio_system_geoloc.py
@@ -1,2 +1,32 @@
+"""
+    geoipy.py
+    A simple IP geolocation python script.
+    Uses geody.com geolocation web service.
+    Requires BeautifulSoup library.
+
+    ksaver, June 18, 2011.
+    Public Domain Code.
+"""
+
+import re
+import sys
+from urllib import urlopen
+from BeautifulSoup import BeautifulSoup as Soup
+#TODO some global import for all weio functions
+from weio_system_network import myExternalIp
+
 def whereIam() :
     """Where I am function tries to geolocate WEIO board"""
+
+    ipAddress = myExternalIp()
+    geody = "http://www.geody.com/geoip.php?ip=" + ipAddress
+    html_page = urlopen(geody).read()
+    soup = Soup(html_page)
+
+    # Filter paragraph containing geolocation info.
+    paragraph = soup('p')[3]
+
+    # Remove html tags using regex.
+    geo_txt = re.sub(r'<.*?>', '', str(paragraph))
+    return geo_txt[32:].strip()
+    
--- weio_core/weio_system_network.py
+++ weio_core/weio_system_network.py
@@ -1,24 +1,41 @@
+import re
+import urllib
+
 def listAvailableWifiSpots() :
     """List available wifi spots will return list of wifi objects."""
+    print("not yet implemented")
 
 def connectToWifiSpot(wifiObject, password) :
     """Connect to wifi spot will connect to specified wifi network"""
+    print("not yet implemented")
 
 def createAP(ssid, password) :
     """This function creates Access Point network of specified ssid name and password. If password is omitted than network will be free to access"""
-    
+    print("not yet implemented")
+
 def isConnected() :
     """Is connected checks if WEIO is connected to the wifi"""
+    print("not yet implemented")
 
 def myIp() :
     """My Ip returns current ip address of WEIO device on wlan01 interface"""
+    print("not yet implemented")
 
 def defineMyIpAddress(address) :
     """Define my Ip address function defines specified Ip address for wlan01 interfac"""
-    
+    print("not yet implemented")
+
+def discoverOtherWeios() :
+    print("not yet implemented")
+
+def ping(ip, port) :
+    print("not yet implemented")
+
 def myExternalIp():
     """My external Ip returns current Ip address accessible from Internet by pinging external server and returning Ip address"""
-def discoverOtherWeios() :
-    
-def ping(ip, ping) :
-    
+    #TODO : try except this call
+    f = urllib.urlopen("http://www.canyouseeme.org/")
+    html_doc = f.read()
+    f.close()
+    ipaddr = re.search('(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)',html_doc)
+    return str(ipaddr.group(0))
