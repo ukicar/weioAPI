79e529224fc0b47c533547584bc63aaf0e89f87a
uros petrevski <uros@nodesign.net>
2013-02-07 23:56:53 +0100

    added myIp() address code

--- weio_core/weio_system_network.py
+++ weio_core/weio_system_network.py
@@ -1,6 +1,8 @@
 import re
 import urllib
 
+import ping, socket
+ 
 def listAvailableWifiSpots() :
     """List available wifi spots will return list of wifi objects."""
     print("not yet implemented")
@@ -19,7 +21,7 @@ def isConnected() :
 
 def myIp() :
     """My Ip returns current ip address of WEIO device on wlan01 interface"""
-    print("not yet implemented")
+    return [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1]
 
 def defineMyIpAddress(address) :
     """Define my Ip address function defines specified Ip address for wlan01 interfac"""
@@ -28,8 +30,8 @@ def defineMyIpAddress(address) :
 def discoverOtherWeios() :
     print("not yet implemented")
 
-def ping(ip, port) :
-    print("not yet implemented")
+def ping(ip) :   
+     print("not yet implemented")
 
 def myExternalIp():
     """My external Ip returns current Ip address accessible from Internet by pinging external server and returning Ip address"""
